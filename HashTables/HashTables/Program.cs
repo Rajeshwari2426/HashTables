9996b553a0896826d381d0bd16fdc6763f4c2255
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-03-04 22:36:37 +0530

    [Rajeshwari] adding Frequency of words

--- HashTables/HashTables/Program.cs
+++ HashTables/HashTables/Program.cs
@@ -11,6 +11,17 @@ namespace HashTables
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to HashTables Program");
+            MyMapNode<string, string> hash = new MyMapNode<string, string> (5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value:" +hash5);
+            Console.ReadKey();
+
         }
     }
 }
